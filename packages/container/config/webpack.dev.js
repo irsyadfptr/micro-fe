44e14189cf1f516006ede3cfb75a131d39ed8719
Irsyad Fakhri Putranto <irsyad.putranto@sg-edts.com>
2022-12-02 13:45:37 +0700

    set up prod config for container and marketing repos

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,5 @@
 //merge config on common
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -23,9 +22,6 @@ const developmentConfig = {
             },
             shared: packageJson.dependencies
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
     ]
 }
 
