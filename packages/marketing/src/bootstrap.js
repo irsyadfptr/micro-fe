ef58bb8977f32857a3bfc51896c38811e89a43a8
Irsyad Fakhri Putranto <irsyad.putranto@sg-edts.com>
2022-12-04 06:22:00 +0700

    Setting up for skipping aws config module

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+// Mount function to start up the app
 const mount = (el) => {
     ReactDOM.render(<App/>, el);
 }
 
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
     const el = document.querySelector('#_marketing-dev-root');
     if (el) {
@@ -13,4 +16,6 @@ if (process.env.NODE_ENV === 'development') {
     }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
