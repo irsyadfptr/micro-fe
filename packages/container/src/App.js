849e64488e86c256fb270b717b6a4c64ae97534e
Irsyad Fakhri Putranto <irsyad.putranto@sg-edts.com>
2022-12-04 10:22:46 +0700

    Fix css on prod for future uses

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default () => {
     return(
-        <div>
-            <h1>Irsyad Here!</h1>
-            <hr />
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
     )
 }
