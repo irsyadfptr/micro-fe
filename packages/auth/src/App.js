51763f29136dd61c0409c2dbf89ea013b9660d86
Irsyad Fakhri Putranto <irsyad.putranto@sg-edts.com>
2022-12-05 14:00:16 +0700

    Add auth module on microfrontend

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Switch, Route, Router } from 'react-router-dom'
+import { Switch, Route, Router, BrowserRouter } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+import SignUp from './components/Signup'
+import SignIn from './components/Signin'
 
-import Landing from './components/Landing'
-import Pricing from './components/Pricing'
+
 
 const generateClassName = createGenerateClassName({
-    productionPrefix: 'ma'
+    productionPrefix: 'au'
 })
 
 
@@ -16,8 +17,8 @@ export default ({history}) => {
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history}>
                     <Switch>
-                        <Route exact path="/pricing" component={Pricing} />
-                        <Route path="/" component={Landing} />
+                        <Route path="/auth/signin" component={SignIn} />
+                        <Route path="/auth/signup" component={SignUp} />
                     </Switch>
                 </Router>
             </StylesProvider>
